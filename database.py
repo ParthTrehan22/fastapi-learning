1b3e37ac29141cf1b00dc260f239a73f4ec6c74d
ParthTrehan <parthtrehan221997@gmail.com>
2026-08-05 22:42:46 +0530

    removed secrets

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:parthtrehan123@db.kudejfnhsqyuvyiftscb.supabase.co:5432/postgres"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
